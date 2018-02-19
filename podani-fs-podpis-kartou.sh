e93c2a8422f2a73e35e3d8942dadb9d6151261b2
František Řezáč <frantisek.rezac@calavera.info>
2018-02-19 22:47:00 +0100

    Pridej parametr -binary jen kdyz se komprimuje obsah

--- podani-fs-podpis-kartou.sh
+++ podani-fs-podpis-kartou.sh
@@ -4,13 +4,15 @@
 
 DOCUMENT=`zenity --file-selection --title="Dokument k podpisu"`
 CERTIFICATE=`zenity --file-selection --title="Podpisový certifikát"`
+COMPRESS=
 
 if $(zenity --question --text="Má se dokument před podpisem komprimovat?\nNěkterá podání se komprimovat nemusí (přiznání DPH),\nněkterá jsou komprimovaná povinně (kontrolní hlášení).") ; then
 	zip --junk-paths --compression-method deflate "$DOCUMENT.zip" "$DOCUMENT"
+	COMPRESS="-binary"
 	DOCUMENT="$DOCUMENT.zip"
 fi
 
 openssl <<HereDoc
 engine dynamic -pre SO_PATH:/usr/lib/x86_64-linux-gnu/engines-1.1/pkcs11.so -pre ID:pkcs11 -pre NO_VCHECK:1 -pre LIST_ADD:1 -pre LOAD -pre MODULE_PATH:/usr/lib/x86_64-linux-gnu/opensc-pkcs11.so
-smime -sign -engine pkcs11 -in "$DOCUMENT" -binary -out "$DOCUMENT.p7s" -inkey 0:2 -signer "$CERTIFICATE" -keyform engine -outform DER -nodetach
+smime -sign -engine pkcs11 -in "$DOCUMENT" $COMPRESS -out "$DOCUMENT.p7s" -inkey 0:2 -signer "$CERTIFICATE" -keyform engine -outform DER -nodetach
 HereDoc
